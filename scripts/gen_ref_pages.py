98b2c5da455bbe6d510a6009f58e49271a6ffcaf
Sean Stewart <sean_stewart@me.com>
2024-08-04 11:09:20 -0400

    docs: drop nav generation from docs page gen

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -6,7 +6,6 @@ import mkdocs_gen_files
 
 
 def gen_files(source: pathlib.Path) -> None:
-    nav = mkdocs_gen_files.Nav()
     for path in sorted(source.rglob("*.py")):
         module_path = path.relative_to(source).with_suffix("")
         doc_path = path.relative_to(source).with_suffix(".md")
@@ -24,19 +23,13 @@ def gen_files(source: pathlib.Path) -> None:
 
         if module_path.name == "__init__":
             parts = parts[:-1]
-            doc_path = doc_path.with_name("index.md")
             full_doc_path = full_doc_path.with_name("index.md")
-
-        nav[parts] = doc_path.as_posix()
 
         with mkdocs_gen_files.open(full_doc_path, "w") as fd:
             ident = ".".join(parts)
             fd.write(f"::: {ident}")
 
         mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
-
-    with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
-        nav_file.writelines(nav.build_literate_nav())
 
 
 root = pathlib.Path(__file__).parent.parent
